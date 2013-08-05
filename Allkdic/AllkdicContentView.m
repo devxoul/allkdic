69c3cd8fb15b8cada58a42a6b6e0a020de476944
전수열 <devxoul@gmail.com>
2013-08-06 03:33:30 +0900

    Changed bundle name.

--- Allkdic/AllkdicContentView.m
+++ Allkdic/AllkdicContentView.m
@@ -12,13 +12,31 @@
 
 - (id)init
 {
-	self = [super initWithFrame:NSMakeRect( 0, 0, 405, 513 )];
+	self = [super initWithFrame:NSMakeRect( 0, 0, 405, 513+38 )];
 	
-	self.webView = [[WebView alloc] initWithFrame:self.frame];
+	self.webView = [[WebView alloc] initWithFrame:NSMakeRect( 0, 0, 405, 513 )];
 	self.webView.mainFrameURL = @"http://endic.naver.com/popManager.nhn?m=miniPopMain";
 	[self addSubview:self.webView];
 	
-	[self.webView becomeFirstResponder];
+	NSImageView *line = [[NSImageView alloc] initWithFrame:NSMakeRect( 0, self.frame.size.height - 38, 405, 2 )];
+	line.image = [NSImage imageNamed:@"line.png"];
+	[self addSubview:line];
+	
+	NSTextField *titleLabel = [[NSTextField alloc] init];
+	titleLabel.stringValue = BUNDLE_NAME;
+	[titleLabel setAlignment:NSCenterTextAlignment];
+	[titleLabel setEnabled:NO];
+	[titleLabel setEditable:NO];
+	[titleLabel setBordered:NO];
+	[titleLabel setBackgroundColor:[NSColor clearColor]];
+	[titleLabel sizeToFit];
+	titleLabel.frame = NSMakeRect( 0, self.frame.size.height - titleLabel.frame.size.height - 9, 405, titleLabel.frame.size.height );
+	[self addSubview:titleLabel];
+	
+	NSButton *helpButton = [[NSButton alloc] initWithFrame:NSMakeRect( 375, self.frame.size.height - 31, 25, 25 )];
+	helpButton.title = nil;
+	helpButton.bezelStyle = NSHelpButtonBezelStyle;
+	[self addSubview:helpButton];
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(webviewDidFinishLoading) name:WebViewProgressFinishedNotification object:nil];
 	
---
+++ Allkdic/Common.h
@@ -0,0 +1,11 @@
+//
+//  Common.h
+//  Allkdic
+//
+//  Created by 전수열 on 13. 8. 6..
+//  Copyright (c) 2013년 Joyfl. All rights reserved.
+//
+
+#define VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
+#define BUILD [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
+#define BUNDLE_NAME [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
